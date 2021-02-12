b36ef45061369794c82e6235cc8ab1a77be682cd
Ashwin (silipwn) <mail@as-hw.in>
2021-02-12 16:50:35 +0530

    [Compile]: Add support for DuT and Ref
    - Separate compile into DuT and Ref modes
    - Adds compare function to compare logs between DuT and Ref plugins
    (Thanks to @pawks)

--- river_core/rivercore.py
+++ river_core/rivercore.py
@@ -5,6 +5,7 @@ import shutil
 import yaml
 import importlib
 import configparser
+import difflib
 
 from river_core.log import *
 from river_core.utils import *
@@ -14,6 +15,42 @@ from river_core.sim_hookspecs import *
 import riscv_config.checker as riscv_config
 from riscv_config.errors import ValidationError
 from envyaml import EnvYAML
+
+
+# Misc Helper Function
+def compare_signature(file1, file2):
+    '''
+        Function to check whether two signature files are equivalent. This funcion uses the
+        :py:mod:`difflib` to perform the comparision and obtain the difference.
+
+        :param file1: The path to the first signature.
+
+        :param file2: The path to the second signature.
+
+        :type file1: str
+
+        :type file2: str
+
+        :return: A string indicating whether the test "Passed" (if files are the same)
+            or "Failed" (if the files are different) and the diff of the files.
+
+    '''
+    if not os.path.exists(file1):
+        logger.error('Signature file : ' + file1 + ' does not exist')
+        sys.exit(1)
+    file1_lines = open(file1, "r").readlines()
+    res = ("".join(
+        difflib.unified_diff(file1_lines,
+                             open(file2, "r").readlines(), file1,
+                             file2))).strip()
+    if res == "":
+        if len(file1_lines) == 0:
+            return 'Failed', '---- \nBoth Files empty\n'
+        else:
+            status = 'Passed'
+    else:
+        status = 'Failed'
+    return status, res
 
 
 def rivercore_clean(config_file, output_dir, verbosity):
@@ -176,6 +213,7 @@ def rivercore_compile(config_file, output_dir, asm_dir, verbosity):
 
         path_to_module = config['default']['path_to_target']
         plugin_target = target + '_plugin'
+        logger.info('Now running on the Target Plugins')
         logger.info('Now loading {0}-target'.format(target))
 
         abs_location_module = path_to_module + '/' + plugin_target + '/' + plugin_target + '.py'
@@ -186,9 +224,10 @@ def rivercore_compile(config_file, output_dir, asm_dir, verbosity):
         dutpm_module = importlib.util.module_from_spec(dutpm_spec)
         dutpm_spec.loader.exec_module(dutpm_module)
 
+        # DuT Plugins
         if target == 'chromite':
             dutpm.register(dutpm_module.ChromitePlugin())
-            # Add more plugins here :)
+            # NOTE: Add more plugins here :)
         else:
             logger.error("Sorry, requested plugin is not really supported ATM")
             raise SystemExit
@@ -207,8 +246,51 @@ def rivercore_compile(config_file, output_dir, asm_dir, verbosity):
         # norun=norun,
         # filter=filter)
         dutpm.hook.run(module_dir=path_to_module, asm_dir=asm_dir)
-        dutpm.hook.post_run()
+        target_log = dutpm.hook.post_run()
 
+    ref_list = config['default']['reference'].split(',')
+    for ref in ref_list:
+
+        # compilepm = pluggy.PluginManager('compile')
+        dutpm = pluggy.PluginManager('dut')
+        # compilepm.add_hookspecs(CompileSpec)
+        dutpm.add_hookspecs(DuTSpec)
+
+        path_to_module = config['default']['path_to_ref']
+        plugin_ref = ref + '_plugin'
+        logger.info('Now loading {0}-target'.format(ref))
+
+        abs_location_module = path_to_module + '/' + plugin_ref + '/' + plugin_ref + '.py'
+        logger.debug("Loading module from {0}".format(abs_location_module))
+
+        dutpm_spec = importlib.util.spec_from_file_location(
+            plugin_ref, abs_location_module)
+        dutpm_module = importlib.util.module_from_spec(dutpm_spec)
+        dutpm_spec.loader.exec_module(dutpm_module)
+
+        # DuT Plugins
+        if ref == 'spike':
+            dutpm.register(dutpm_module.SpikePlugin())
+            # NOTE: Add more plugins here :)
+        else:
+            logger.error("Sorry, requested plugin is not really supported ATM")
+            raise SystemExit
+
+        dutpm.hook.init(ini_config=config[ref],
+                        yaml_config=path_to_module + '/' + plugin_ref + '/' +
+                        'config.yaml',
+                        output_dir=output_dir)
+        # NOTE (Add to documentation)
+        # The config files should be saved as config.yaml in the plugin repo
+        dutpm.hook.build(asm_dir=asm_dir, asm_gen=asm_gen)
+        # regress_list='{0}/{1}/regresslist.yaml'.format(
+        # output_dir, suite),
+        # command_line_args='',
+        # jobs=jobs,
+        # norun=norun,
+        # filter=filter)
+        dutpm.hook.run(module_dir=path_to_module, asm_dir=asm_dir)
+        ref_log = dutpm.hook.post_run()
         # except FileNotFoundError as txt:
         #     logger.error(target + " not found at : " + path_to_module + ".\n" +
         #                  str(txt))
@@ -225,3 +307,25 @@ def rivercore_compile(config_file, output_dir, asm_dir, verbosity):
         #compilepm.hook.pre_compile(compile_config='{0}/river_core/chromite_simlog_plugin/chromite_config.yaml'.format(cwd))
         #compilepm.hook.compile(regress_list='{0}/workdir/regresslist.yaml'.format(cwd), command_line_args='', jobs=jobs, filter=filter)
         #compilepm.hook.post_compile()
+
+        # Start comparison between files
+        # TODO Replace with a signature based model
+        logger.info('Starting comparison between logs')
+        # Check if the logs are same number
+        try:
+
+            if len(ref_log) == len(target_log):
+                for i in range(len(ref_log)):
+                    # NOTE This is absolutely strange! Why is a double list is created
+                    status, result = compare_signature(ref_log[0][i],
+                                                       target_log[0][i])
+                    logger.info("Matching {0} and {1} \n Result : {2}".format(
+                        ref_log[i], target_log[i], status))
+            else:
+                logger.info(
+                    'Something is not right with the logs, manual inspection is now suggested.'
+                )
+                raise SystemExit
+        except SystemExit as err:
+            print("Some thing went wrong :|")
+            raise
