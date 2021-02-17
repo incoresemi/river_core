963eb1bf224a4fc84e190acc99fb7320b2e89b53
Ashwin (silipwn) <mail@as-hw.in>
2021-02-17 20:05:33 +0530

    [Utils]: Ensure subprocess is killed
    - To mainly prevent resource exhaustion

--- river_core/utils.py
+++ river_core/utils.py
@@ -6,32 +6,47 @@ import subprocess
 import shlex
 from river_core.log import logger
 import ruamel
+import signal
 from ruamel.yaml import YAML
 from threading import Timer
 
 
-def sys_command(command, timeout=500):
+def sys_command(command, timeout=30):
+    # test = 'exec ' + command
+    # logger.debug(test)
     logger.warning('$ timeout={1} {0} '.format(' '.join(shlex.split(command)),
                                                timeout))
-    x = subprocess.Popen(
-        shlex.split(command),
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    timer = Timer(timeout, x.kill)
-    try:
-        timer.start()
-        out, err = x.communicate()
-    finally:
-        timer.cancel()
+    out = ''
+    err = ''
+    with subprocess.Popen(shlex.split(command),
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE,
+                          start_new_session=True) as process:
+        try:
+            out, err = process.communicate(timeout=timeout)
+        except subprocess.TimeoutExpired:
+            process.kill()
+            pgrp = os.getpgid(process.pid)
+            os.killpg(pgrp, signal.SIGTERM)
+            return 1, "GuruMeditation", "TimeoutExpired"
 
-    #except subprocess.TimeoutExpired:
-    #    x.kill()
-    #    out, err = x.communicate()
+    # timer = Timer(timeout, process.kill)
+    # try:
+    #     timer.start()
+    #     out, err = process.communicate()
+    # finally:
+    #     timer.cancel()
+
+    #eprocess.ept subprocess.TimeoutExpired:
+    #    process.kill()
+    #    out, err = process.communicate()
+
+    # out = process.stdout
+    # err = process.stderr
 
     out = out.rstrip()
     err = err.rstrip()
-    if x.returncode != 0:
+    if process.returncode != 0:
         if out:
             logger.error(out.decode("ascii"))
         if err:
@@ -41,7 +56,7 @@ def sys_command(command, timeout=500):
             logger.debug(out.decode("ascii"))
         if err:
             logger.debug(err.decode("ascii"))
-    return (x.returncode, out.decode("ascii"), err.decode("ascii"))
+    return (process.returncode, out.decode("ascii"), err.decode("ascii"))
 
 
 def sys_command_file(command, filename, timeout=500):
