ba0be9176f0648e64e91e57921ca99d9c73eebd2
Ashwin (silipwn) <mail@as-hw.in>
2021-05-27 18:39:04 +0530

    Remove old 'debug_scripts'

--- debug_scripts/comp_script.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-line=$(cmp "$1" "$2" | awk '{print $NF}')
-if [ ! -z $line ]; then
-    awk -v file="$1" -v line=$line 'NR==line{print "```\n"file":"$0; exit}' "$1"
-    awk -v file="$2" -v line=$line 'NR==line{print ""file":"$0 "\n```"; exit}' "$2"
-fi
